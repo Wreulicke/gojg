4d2727780a1e1eff48ab73909f12828b5ab035bd
wreulicke <saito.masaya@classmethod.jp>
2017-09-04 05:30:14 +0900

    simplify test

--- parse/parser_test.go
+++ parse/parser_test.go
@@ -8,14 +8,20 @@ import (
 )
 
 func TestParseString(t *testing.T) {
-	r, err := Parse(`"test"`)
-	if err != nil {
-		t.Fatal(err)
-	} else if v, ok := r.(*ast.ValueNode); ok {
+	r := MustParse(t, `"test"`)
+	if v, ok := r.(*ast.ValueNode); ok {
 		t.Logf("value: %s", v)
 	} else {
 		t.Fatalf("%s(type: %s) is not value node", r, getTypeName(r))
 	}
+}
+
+func MustParse(t *testing.T, src string) ast.AST {
+	r, err := Parse(src)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return r
 }
 
 func getTypeName(o interface{}) string {
