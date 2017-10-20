2059ad027e64a3da1b2ac6ef7b9d61ab6d0db257
wreulicke <saito.masaya@classmethod.jp>
2017-10-21 00:50:19 +0900

    Implement Simple CLI

--- main.go
+++ main.go
@@ -2,21 +2,48 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 
+	"github.com/alecthomas/kingpin"
 	"github.com/wreulicke/gojg/generator"
 	"github.com/wreulicke/gojg/parse"
 )
 
+var (
+	verbose  = kingpin.Flag("verbose", "Set verbose mode").Short('v').Bool()
+	template = kingpin.Arg("template", "Template File").Required().String()
+)
+
 func main() {
-	ast, e := parse.Parse("true")
+	kingpin.Version("0.0.1")
+	kingpin.CommandLine.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	f, e := os.Open(*template)
 	if e != nil {
-		return
+		fmt.Println(e)
+		os.Exit(1)
 	}
+
+	reader := bufio.NewReader(f)
+	ast, e := parse.Parse(reader)
+	if e != nil {
+		f.Close()
+		fmt.Println("error1")
+		os.Exit(1)
+	}
+
 	writer := bufio.NewWriter(os.Stdout)
 	err := generator.Generate(ast, writer)
+	writer.WriteRune('\n')
+	writer.Flush()
 	if err != nil {
-		return
+		f.Close()
+		fmt.Println(e)
+		os.Exit(1)
 	}
+
+	writer.WriteRune('\n')
 	writer.Flush()
 }
